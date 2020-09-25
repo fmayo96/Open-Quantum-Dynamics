dd332b65418852923f45599da07678b1026992f3
Franco <francomayo96@gmail.com>
2020-09-25 13:30:50 -0300

    fixed energy - missing heat

--- energy.f90
+++ energy.f90
@@ -2,17 +2,16 @@ subroutine Energy(E, state, hamiltonian, dim)
     integer :: dim 
     complex, dimension(dim,dim) :: state, hamiltonian, prod
     real :: E
-    call dot(prod, state, hamiltonian,dim)
+    call dot(prod, state, hamiltonian, dim)
     call trace(E, prod, dim)
 end subroutine Energy
 
-subroutine Heat(Q, state, bath_state, bath_hamiltonian, interaction, dt, dim, i, N)
+subroutine Heat(delta_Q, state, bath_state, bath_hamiltonian, interaction, dim)
     integer :: dim 
     complex, dimension(dim,dim) :: state, bath_state, bath_hamiltonian, eye
-    complex, dimension(dim**2, dim**2) :: interaction, prod_state, prod_hamiltonian, comm
-    complex, dimension(dim**2, dim**2) :: double_comm, dot_product_state_double_comm
-    real :: delta_Q, dt
-    real, dimension(N) :: Q
+    complex, dimension(dim**2, dim**2) :: interaction, prod_state, prod_hamiltonian
+    complex, dimension(dim**2, dim**2) :: comm, double_comm, dot_product_state_double_comm
+    real :: delta_Q
     eye(1,1) = 1
     eye(1,2) = 0
     eye(2,1) = 0
@@ -23,7 +22,7 @@ subroutine Heat(Q, state, bath_state, bath_hamiltonian, interaction, dt, dim, i,
     call commutator(double_comm, interaction, comm, dim**2)
     call dot(dot_product_state_double_comm, prod_state, double_comm, dim**2)
     call trace(delta_Q, dot_product_state_double_comm, dim**2)
-    Q(i + 1) = Q(i) + 0.5 * dt * delta_Q
+
 end subroutine Heat 
 
 subroutine Work(W, E, Q)
--- time_evolution.f90
+++ time_evolution.f90
@@ -1,15 +1,12 @@
 program test
+    implicit none
     complex, dimension(2,2) :: state, hamiltonian, bath_state, bath_hamiltonian
     complex, dimension(4,4) :: interaction
     real :: tf, dt, h, eps, beta
     integer :: N 
-    real, dimension(:), allocatable :: E, W, Q     
     dt = 0.001
     tf = 10
     N = int(tf/dt)
-    allocate(E(N))
-    allocate(Q(N))
-    allocate(W(N))
     h = 1.5
     eps = 0.5**0.5
     beta = 1
@@ -31,19 +28,24 @@ program test
     call Open_evolution(state, hamiltonian, bath_state, bath_hamiltonian, interaction, N, dt, 2)
 end program 
 subroutine Open_evolution(state, hamiltonian, bath_state, bath_hamiltonian, interaction, N, dt, dim)
-    integer :: dim, i, N, j 
+    integer :: dim, i, N 
     complex, dimension(dim,dim) :: state, hamiltonian, bath_state, bath_hamiltonian
     complex, dimension(dim**2, dim**2) :: interaction
-    real :: tf, dt
+    real :: dt, delta_Q
     real, dimension(N) :: E, Q, W
     Q = 0
     E = 0
     W = 0
-    open(1, file = 'test.txt', status = 'new')
-    do i = 1, N-1 
+    
+    !open(1, file = 'test_E.txt', status = 'new')
+    do i = 1, N - 1
         call RK4_open(state, hamiltonian, bath_state, interaction, dt, dim)
-        write (1,*) Real(state(1,1)), Real(state(2,2))
-        
+    !    write (1,*) Real(state(1,1)), Real(state(2,2))
+        call Energy(E(i), state, hamiltonian, 2)
+        call Heat(delta_Q, state, bath_state, bath_hamiltonian, interaction, 2)     
+        Q(i + 1) = Q(i) + 0.5 * dt * delta_Q
+        !write (1,*) E(i), Q(i)
+        print *, Q(i)
     end do
-    close(1)
+    !close(1)
 end subroutine Open_evolution
